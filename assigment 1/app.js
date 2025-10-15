6e5f6bc549a7aad7e786f76c7db7ef32d2aad371
Muhammad Umar Hassan <umarrhassan321@gmail.com>
2025-10-15 14:29:59 +0500

    Add files via upload

---
+++ assigment 1/app.js
@@ -0,0 +1,39 @@
+console.log("Hello..i can JS through my web browser's console ")
+alert("please enter a valid password")
+
+alert("Hello User")
+
+alert("Welcome To JS Land..\n Happy Coding" )
+alert("welcome to js land...")
+alert("prevent this page from creating additional dialogs ")
+
+// chapter 2
+
+let username 
+let myname = "Muhammad Umar Hassan"
+let age = "I am 18 year old"
+let certificate = "web development"
+
+alert(myname)
+alert(age)
+alert(certificate)
+
+var pizza = "\n pizza \n pizz \n piz \n pi \n p"
+alert(pizza)
+
+let email="umarrhassan321@gmail.com"
+alert(email)
+
+let msg="A smarter way to learn javascript"
+alert(msg)
+
+let visited ="You have visited this site 10 times"
+alert(visited)
+
+document.writeln("<h1> Yah! I can write HTML content through Javascript </h1>")
+
+let end="...END..."
+alert(end)
+
+document.writeln("<h1> My birth year is 2007 </h1>")
+document.writeln("<h1> Data Type of my declared variable is number </h1>")
